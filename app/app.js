2317c86040dd2609f612ba13f2a51ab419e51d27
Natalia Alfaro <natalia.alfaro@truextend.com>
2017-05-16 15:32:25 -0400

    first commit

--- app/app.js
+++ app/app.js
@@ -1,14 +1,44 @@
-'use strict';
-
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+(function () {
+    'use strict';
+ 
+    angular
+        .module('app', ['ui.router', 'ngMessages', 'ngStorage','angularUUID2'])
+        .config(config)
+        .run(run);
+ 
+    function config($stateProvider, $urlRouterProvider) {
+        // default route
+        $urlRouterProvider.otherwise("/");
+ 
+        // app routes
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'home/index.view.html',
+                controller: 'Home.IndexController',
+                controllerAs: 'vm'
+            })
+            .state('login', {
+                url: '/login',
+                templateUrl: 'login/index.view.html',
+                controller: 'Login.IndexController',
+                controllerAs: 'vm'
+            });
+    }
+ 
+    function run($rootScope, $http, $location, $localStorage) {
+        // keep user logged in after page refresh
+        if ($localStorage.currentUser) {
+            $http.defaults.headers.common.Authorization = 'Bearer ' + $localStorage.currentUser.token;
+        }
+ 
+        // redirect to login page if not logged in and trying to access a restricted page
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            var publicPages = ['/login'];
+            var restrictedPage = publicPages.indexOf($location.path()) === -1;
+            if (restrictedPage && !$localStorage.currentUser) {
+                $location.path('/login');
+            }
+        });
+    }
+})();
