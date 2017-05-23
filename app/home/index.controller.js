97142b0f0d5bfcb0fe77bfd13ecec3a806cc12cb
Natalia Alfaro <natalia.alfaro@truextend.com>
2017-05-23 12:27:29 -0400

    loading user data

--- app/home/index.controller.js
+++ app/home/index.controller.js
@@ -5,12 +5,43 @@
         .module('app')
         .controller('Home.IndexController', Controller);
  
-    function Controller() {
+    function Controller($http, urls, $scope) {
         var vm = this;
  
         initController();
  
         function initController() {
+             $http.get(urls.BASE_API +'/me')
+                .success(function (response) {
+                   if(response.isParent){
+                        getParent(response.userId);
+                   }else{
+                        getKid(response.userId);
+                   }
+                })
+                .error(function(err){
+                    console.log(err);
+                });
+        }
+
+        function getParent(userId){
+            $http.get(urls.BASE_API +'/parents/'+userId)
+                .success(function (response) {
+                    $scope.user=response.parent;
+                })
+                .error(function(err){
+                    console.log(err);
+                });
+        }
+
+        function getKid(userId){
+            $http.get(urls.BASE_API +'/kids/'+userId)
+                .success(function (response) {
+                    $scope.user=response.kid;
+                })
+                .error(function(err){
+                    console.log(err);
+                });
         }
     }
 })();
